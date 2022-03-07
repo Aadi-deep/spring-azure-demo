00c5484ba7dd0738427e281f79736cd89d2f4a90
Aadi-deep <Aaditya.ip@wissen.com>
2022-03-07 17:57:30 +0530

    second commit

--- src/main/java/com/cloudcoe/testspringbootapp/TestSpringbootAppApplication.java
+++ src/main/java/com/cloudcoe/testspringbootapp/TestSpringbootAppApplication.java
@@ -2,9 +2,18 @@ package com.cloudcoe.testspringbootapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TestSpringbootAppApplication {
+
+	@GetMapping("/message")
+	public String message()
+	{
+		return "Congrats! Your application deployed successfully on Azure Platform. ";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestSpringbootAppApplication.class, args);
